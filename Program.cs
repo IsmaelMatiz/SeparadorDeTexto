bba4abd97346515b2c8583bd534dbf2a9e800b69
IsmaelMatiz <ismaelmatiz@gmail.com>
2023-07-27 06:12:53 -0500

    El programa terminado en una primera version, siempre hay campo para mejoras

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "C:\\Users\\PC-89091\\Documents\\OneDrive_2023-07-25\\Table Fields";
+        string resultPath = "C:\\Users\\PC-89091\\Documents\\OneDrive_2023-07-25\\Table Fields por comas";
+        var files = Directory.GetFiles(path, "*.txt");
+        string[] text;
+        String phrase;
+        String eachFileName;
+        String[] newText;
+        foreach (var item in files)
+        {
+            text = File.ReadAllLines(item);
+            eachFileName = Path.GetFileName(item);
+            phrase = text[0];
+            newText = phrase.Split('\t');
+            phrase = String.Join(",", newText)+Environment.NewLine;
+            File.AppendAllText(resultPath+$"\\{eachFileName}", phrase);
+        }
+        Console.WriteLine("Done");
+    }
+}
